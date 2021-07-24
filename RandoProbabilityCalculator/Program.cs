67ca1d7980b8052c018061be1b3b5b5b5f13095f
HapaxL <hapax.legomenon@outlook.fr>
2021-07-24 04:25:18 +0200

    Add project files.

---
+++ RandoProbabilityCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandoProbabilityCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
