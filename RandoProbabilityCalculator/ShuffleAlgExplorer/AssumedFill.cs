0bb5dd992312225d657a20c73bb0f63e4da74f41
HapaxL <hapax.legomenon@outlook.fr>
2021-09-08 01:00:56 +0200

    fixed parent display for failed outcomes.

--- RandoProbabilityCalculator/ShuffleAlgExplorer/AssumedFill.cs
+++ RandoProbabilityCalculator/ShuffleAlgExplorer/AssumedFill.cs
@@ -51,7 +51,7 @@ namespace RandoProbabilityCalculator.ShuffleAlgExplorer
 
             if (reachableEmptyLocs.Count() == 0)
             {
-                var compiled = CompileSingleOutcome(ocCount, Outcome.Failed, new Dictionary<string, long> { { parent, 1 } });
+                var compiled = CompileSingleOutcome(ocCount, Outcome.Failed, new Dictionary<string, long> { { outcome.GetWorldString(0), 1 } });
                 return compiled;
             }
 
